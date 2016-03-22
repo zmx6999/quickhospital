44179df9e6d9e5af65c568a955152bb27a3c09ca
zmx6999 <2455847459@qq.com>
2016-03-22 04:57:19 -0700

    Initial Commit

---
+++ QuickHospital/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QuickHospital
+//
+//  Created by zmx on 16/3/22.
+//  Copyright © 2016年 zmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QuickHospital/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QuickHospital
+//
+//  Created by zmx on 16/3/22.
+//  Copyright © 2016年 zmx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
